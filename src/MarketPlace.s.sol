e47032d89395a6dbc771a34fe4e69c6f82c393c8
nouman <noumankhan95@yahoo.com>
2025-08-01 16:09:34 +0500

    logic for buying being written

--- src/MarketPlace.s.sol
+++ src/MarketPlace.s.sol
@@ -1,9 +1,11 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.18;
-import {NFT} from "./NFT.s.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
-contract MarketPlace is NFT {
+contract MarketPlace {
     error MarketPlace__OnlyOwnerCanList();
     event NFTListed(
         address indexed owner,
@@ -15,32 +17,44 @@ contract MarketPlace is NFT {
         address owner;
         uint256 price;
     }
-    mapping(address => mapping(uint256 => bool)) public listings;
-    mapping(uint256 => Listing) public nftListings;
-    modifier onlyOwnerCanListOrDelist(uint256 tokenId) {
+    mapping(address => mapping(uint256 => Listing)) public listings;
+    ERC20 public paymentToken;
+    modifier onlyOwnerCanListOrDelist(uint256 tokenId, address _nftContract) {
         require(
-            _ownerOf(tokenId) == msg.sender,
+            IERC721(_nftContract).ownerOf(tokenId) == msg.sender,
             MarketPlace__OnlyOwnerCanList()
         );
         _;
     }
 
+    constructor(ERC20 _paymentToken) {
+        paymentToken = _paymentToken;
+    }
+
     function listNFT(
         uint256 _tokenId,
-        uint256 _price
-    ) public onlyOwnerCanListOrDelist(_tokenId) {
-        listings[msg.sender][_tokenId] = true;
-        nftListings[_tokenId] = Listing(msg.sender, _price);
+        uint256 _price,
+        address _nftContract
+    ) public onlyOwnerCanListOrDelist(_tokenId, _nftContract) {
+        listings[_nftContract][_tokenId] = Listing(msg.sender, _price);
         emit NFTListed(msg.sender, _tokenId, _price);
     }
 
     function delistNFT(
-        uint256 _tokenId
-    ) public onlyOwnerCanListOrDelist(_tokenId) {
-        listings[msg.sender][_tokenId] = false;
-        delete nftListings[_tokenId];
+        uint256 _tokenId,
+        address _nftContract
+    ) public onlyOwnerCanListOrDelist(_tokenId, _nftContract) {
+        delete listings[_nftContract][_tokenId];
         emit NFTDeListed(msg.sender, _tokenId);
     }
 
-    function buyNFT() public {}
+    function buyNFT(uint256 _tokenid, address _nftContract) public {
+        uint256 price = listings[_nftContract][_tokenid].price;
+        address owner = listings[_nftContract][_tokenid].owner;
+        bool success = paymentToken.transferFrom(owner, msg.sender, price);
+        if (!success) {
+            revert("Payment failed");
+        }
+        IERC721(_nftContract).safeTransferFrom(owner, msg.sender, _tokenid);
+    }
 }
